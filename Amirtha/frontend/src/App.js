54e1e9ffadcc6ddc36a381604489c84d2fd46878
KonguBharath <bharathviswa06@gmail.com>
2023-04-16 07:53:32 +0530

    header

---
+++ Amirtha/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href='/'>Amirtha Cones</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
